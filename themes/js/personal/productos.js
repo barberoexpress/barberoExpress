67e14a5e69d4a1d04cc30568b20e1aa4a3073134
stiag0 <stiago01r@gmail.com>
2017-08-25 22:36:32 -0500

    se quitaron los botones de añadir al carrito, se dejaron solamente en el drtalles producto

--- themes/js/personal/productos.js
+++ themes/js/personal/productos.js
@@ -12,7 +12,7 @@ firebase.initializeApp(config);*/
 //var firebaseAuth = firebase.auth();
 
 
-  
+
 
     var pagina_actual = 1;
     var registros_por_pagina = 6;
@@ -63,10 +63,10 @@ firebase.initializeApp(config);*/
         imagenes += '<li class="span3">';
     		imagenes += '<div class="thumbnail">';
     		imagenes +=	'<img src="'+foto_Url[j]+'" onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')" style="width:200px;height:200px;" alt=""/>';
-    		imagenes +=	'<div class="caption">';                                    
+    		imagenes +=	'<div class="caption">';
     		imagenes +=	'<h5>'+ nombre[j] +'</h5>';
     		imagenes +=	'<p>'+ descripcion[j] +'</p>';
-    		imagenes +=	'<h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')">Añadir <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&dollar;'+precio[j]+'</a></h4>';
+    		imagenes +=	'<h4 style="text-align:center"> <a class="btn" onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')"> <i class="icon-zoom-in"></i> <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&dollar;'+precio[j]+'</a></h4>';
     		imagenes +=	'</div>';
     		imagenes +=	'</div>';
     		imagenes += '</li>';
@@ -117,7 +117,7 @@ function cambiar_pagina(pagina_actual){
       imagenes += '<li class="span3">';
       imagenes += '<div class="thumbnail">';
       imagenes += '<img src="'+foto_Url[j]+'" onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')" style="width:200px;height:200px;" alt=""/>';
-      imagenes += '<div class="caption">';                                    
+      imagenes += '<div class="caption">';
       imagenes += '<h5>'+ nombre[j] +'</h5>';
       imagenes += '<p>'+ descripcion[j] +'</p>';
       imagenes += '<h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')">Add to <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&dollar;'+precio[j]+'</a></h4>';
--- themes/js/personal/productos_index.js
+++ themes/js/personal/productos_index.js
@@ -1,4 +1,4 @@
-  
+
 
   var imagenes = '<ul class="thumbnails" id= "fotos_index">';
   var rows = 1;
@@ -31,10 +31,10 @@
             imagenes += '<li class="span3">';
             imagenes += '<div class="thumbnail">';
             imagenes += '<img src="'+foto_Url[j]+'" onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')" style="width:200px;height:200px;" alt=""/>';
-            imagenes += '<div class="caption">';                                    
+            imagenes += '<div class="caption">';
             imagenes += '<h5>'+ nombre[j] +'</h5>';
             imagenes += '<p> Tocamela toda </p>';
-            imagenes += '<h4 style="text-align:center"><a class="btn" href="product_details.html"> <i class="icon-zoom-in"></i></a> <a class="btn" href="#">Añadir <i class="icon-shopping-cart"></i></a> <a class="btn btn-primary" href="#">&dollar;'+precio[j]+'</a></h4>';
+            imagenes += '<h4 style="text-align:center"> </a>  <a class="btn btn-primary" h> &dollar;'+precio[j]+'</a></h4>';
             imagenes += '</div>';
             imagenes += '</div>';
             imagenes += '</li>';
@@ -59,7 +59,7 @@
                 imagenes_referidos += '<div class="thumbnail">';
                 imagenes_referidos += '<i class="tag"></i>';
                 imagenes_referidos += '<img src="'+foto_Url[j]+'" onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')" style="width:200px;height:200px;" alt=""/>';
-                imagenes_referidos += '<div class="caption">';                                    
+                imagenes_referidos += '<div class="caption">';
                 imagenes_referidos += '<h5>'+ nombre[j] +'</h5>';
                 imagenes_referidos += '<h4 onclick="Ir_producto('+"'"+ keyProducto[j]+"'"+')"><a class="btn"> Ver </a><span class="pull-right">$'+precio[j]+'</span></h4>';
                 imagenes_referidos += '</div>';
