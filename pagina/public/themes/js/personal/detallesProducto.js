0c4e48aed2cfb3f22a60db229115feb7e85c6741
josewweee <josemariamira@hotmail.com>
2017-09-19 18:47:49 -0500

    1.Se agrego la funcion de correr el icono del buscador en todos los views (HTML). 2. en detallesProductos.JS se cambio la forma como se recibia el valor del id del producto: ahora se toma de una variable, y si cuando carga la pagina la variable no esta se toma del HTML como antes

--- pagina/public/themes/js/personal/detallesProducto.js
+++ pagina/public/themes/js/personal/detallesProducto.js
@@ -13,21 +13,19 @@ var prodKey = localStorage.getItem("PROD_KEY");
 var SearchRef = firebase.database().ref("PRODUCTOS/" + prodKey);
 var userkey = localStorage.getItem("USERKEY2");
 var refCarro = firebase.database().ref("USUARIOS/" + userkey + "/" + "carritoCompras");
+var prod_id;
 
 SearchRef.on("value", function(snapshot) {
 	//BASICO
-	console.log(snapshot.val().foto);
 	document.getElementById("nombreProducto").innerHTML = snapshot.val().nombre;
 	document.getElementById("nombreProducto2").innerHTML = snapshot.val().nombre;
 	document.getElementById("marcaProducto").innerHTML = snapshot.val().marca;
 	document.getElementById("descripcion_producto").innerHTML = snapshot.val().descripcion;
   //nota: añadir separadores para el precio (1 000.000)
 	document.getElementById("precioProducto").innerHTML = "<span>" + "$ " + snapshot.val().precio + "</span>";
-  //id del producto NOTA : hay que esconderlo mejor
   document.getElementById("id_Producto").innerHTML = snapshot.val().id;
+  prod_id = snapshot.val().id;
 
-  //solo con esta linea de codigo mal ya no podia obtener mas elementos by id
-  //document.getElementById().innerHTML ("idProducto").innerHTML = snapshot.val().id;
 
   //IMAGENES
 	document.getElementById("imagenProducto1").innerHTML = '<img src="'+snapshot.val().foto+'" id="imagenProducto" style="width:100%" alt=""/>'
@@ -41,31 +39,29 @@ SearchRef.on("value", function(snapshot) {
 
 
 function BuscarEnCarrito(id){
-  console.log("entro");
   var Encontrado = false;
   console.log("id: " + id);
   refCarro.orderByChild("id").equalTo(id).on("child_added",function(snapshot){
-    console.log("encontro");
     Encontrado = true;
-    console.log("Encontrado = "+Encontrado);
     return Encontrado;
   });
-  console.log("Encontrado = "+Encontrado);
   return Encontrado; 
 }
 
 function CambiarAPorIr(uno,dos) {
-//lo de los intentos deberia de estar cada vez que se conecte a Firebase, con valores por defecto
 
-var noMasIntentos = false;
-var intentos = 4;
-var respuesta;
-  var valor = document.getElementById("id_Producto").innerText;
+  var noMasIntentos = false;
+  var intentos = 4;
+  var respuesta;
+  var valor;
+  if(prod_id == null){
+    valor = document.getElementById( "id_Producto" ).innerText;
+  }else{
+    valor = prod_id;
+  }
   console.log(valor);
 
   var esta = BuscarEnCarrito(parseInt(valor));
-
-  console.log(esta);
 
   if (esta == true) {
     noMasIntentos = true;
@@ -79,12 +75,9 @@ var respuesta;
 function switchDivS(id1, id2) {
 //este solo mustra el primero y oculta el segundo
 //facta organizar la posicion, quizas superponer uno ensima de otro
-
-  console.log(id1);
-  console.log(id2);
-
   document.getElementById(id1).style.visibility ="visible";
   document.getElementById("yaLoTiene").style.marginTop ="-80px";
+  document.getElementById("yaLoTiene").style.marginBottom ="50px";
   document.getElementById(id2).style.visibility ="hidden";
 
 }
