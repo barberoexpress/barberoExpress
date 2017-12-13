4442038c49ae3722c10a91f5a65b3411f86b92f8
stiag0 <stiago01r@gmail.com>
2017-12-13 18:19:56 -0500

    se agrego facturacion desde firebase functions

--- backEnd/functions/index.js
+++ backEnd/functions/index.js
@@ -125,7 +125,7 @@ exports.totalizarCarrito = functions.database
 	*/
 	// aqui modificamos el valor vaciar
 	//carritoCompra.vaciar.isVerified = true
-	console.log("entro  >:v")
+	console.log("------- ENVIADO PEDIDO -------------")
 
 
 	var eventSnapshot = event.data; // Get usuario data
@@ -149,23 +149,109 @@ exports.totalizarCarrito = functions.database
 				comoLlegar: adressSnapshot.val().informacionAdicional
 				};
 			info.nombreUsuario = usuario.nombre + " " + usuario.apellido;
-
-
+		var factura ={
+			id:0,
+			nombreUsuario: "",
+			totalPesos: 0,
+			productos: productos
+		}
+		factura.nombreUsuario = usuario.nombre + " " + usuario.apellido;
     itemsSnapshot.forEach(function(itemSnapshot) { // For each item
         //var itemKey = itemSnapshot.key; // Get item key
         var itemData = itemSnapshot.val(); // Get item data
-        console.log("id: "+ itemData.id)
-        console.log("precio: "+ itemData.precio)
+
         var yave = itemData.key;
 
-        console.log("llave: " + yave)
-        /* //pasar de "," a "." en float
-        var values = itemData.precio.split(",")
-		var v1 = parseFloat(values[0])
-		var v2 = parseFloat(values[1])
-		var value = parseFloat(itemData.precio.replace(",", "."));
-		console.log("float: "+ value)
-		*/	
+		var value = parseFloat(itemData.precio);
+		
+		
+		// ---- DATOS INSEGUROS -------
+		var cantidad = parseInt(itemData.cantidad);
+		var nombre = itemData.nombre;
+		var marca = itemData.marca;
+		
+		var producto ={
+			nombre: nombre,
+			marca: marca,
+			cantidad: cantidad,
+			precio: value
+		};
+		
+		productos.push(producto);
+
+		/*
+		pedido.child("productos").push({
+			nombre: nombre,
+			marca: marca,
+			cantidad: cantidad,
+			precio: value
+		});
+		*/
+		totalSD += (value * cantidad);
+		
+
+
+		//aqui mas adelante hacer descuento
+
+    });
+	// cambiamos la informacion del DataBase por el nuevo carritoCompras
+	
+	shoppingCar.total = totalSD;
+
+	info.totalPesos = totalSD;
+	
+	const promise = event.data.ref.set(usuario)
+
+	var ref = event.data.ref.root;
+  	return ref.child("PEDIDOS").push({productos:productos,info:info});
+  	//return ref.child("PEDIDOS").push(info);
+	return promise
+})
+
+
+/*FACTURAS*/
+
+
+exports.crearFactura = functions.database
+.ref('/USUARIOS/{USUARIOSID}')
+.onWrite(event =>{ 
+	// esto es para coger la infurmacion de todo el carrito de compras
+	const usuario = event.data.val()
+	//console.log(carritoCompra.vaciar.isVerified)
+	// preguntamos por vaciar, para que no entre 2 veces
+	/*
+	if (carritoCompra.vaciar.isVerified == 'true') {
+		console.log("se fue")
+		return
+	}
+	*/
+	// aqui modificamos el valor vaciar
+	//carritoCompra.vaciar.isVerified = true
+	console.log("------ FACTURANDO -----")
+
+
+	var eventSnapshot = event.data; // Get usuario data
+    var itemsSnapshot = eventSnapshot.child('carritoCompras/productos'); // Get items data
+    var totalSD = 0 ;
+    var adressSnapshot = eventSnapshot.child('direccion');
+    var phoneSnapshot = eventSnapshot.child('telefono');
+    var shoppingCar = eventSnapshot.child('carritoCompras'); // Get items data
+
+    // --- Pedido ----
+
+		var productos =[];
+		var factura ={
+			id:0,
+			nombreUsuario: "",
+			totalPesos: 0,
+			productos: productos
+		}
+
+		factura.nombreUsuario = usuario.nombre + " " + usuario.apellido;
+    	itemsSnapshot.forEach(function(itemSnapshot) { // For each item
+        //var itemKey = itemSnapshot.key; // Get item key
+        var itemData = itemSnapshot.val(); // Get item data
+        var yave = itemData.key;
 		var value = parseFloat(itemData.precio);
 		
 		/*
@@ -208,35 +294,13 @@ exports.totalizarCarrito = functions.database
 		//aqui mas adelante hacer descuento
 
     });
-    console.log("Total: " + totalSD)
 	// cambiamos la informacion del DataBase por el nuevo carritoCompras
 	
-	shoppingCar.total = totalSD;
+	factura.totalPesos = totalSD;
 
-	info.totalPesos = totalSD;
-	
-	console.log("nombreU: " + info.nombreUsuario)
-	const promise = event.data.ref.set(usuario)
-	console.log("promise Enviado")
-	//var refUsuario = shoppingCar.keyUsuario
-	//console.log("refUsuario: "+refUsuario);
-
+	factura.productos = productos
 	var ref = event.data.ref.root;
-	console.log("ref "+ ref)
-  	return ref.child("PEDIDOS").push({productos:productos,info:info});
-  	console.log("enviado")
-  	//return ref.child("PEDIDOS").push(info);
+	console.log("factura " + factura.nombreUsuario)
+  	return ref.child("FACTURAS").push({factura: factura});
 	return promise
 })
-/*
-function getDatosPadre(event) {
-	coevent.data.ref.parent.once("value").then(snap => {
-      const post = snap.val();
-      // do stuff with post here
-      info.nombreUsuario = post.nombre +" "+ post.apellido;
-      info.telefonoContacto = post.telefono.telefonoCelular;
-      info.direccionEntrega = post.direccion.direccion;
-      info.comoLlegar = post.direccion.informacionAdicional;
-    });
-}
-*/
