7a6f6f37665199863b65570b38aba5f4990fc844
josewweee <josemariamira@hotmail.com>
2017-12-15 16:24:13 -0500

    Añadiendo login con google, puede tener posibles bugs

--- public/js/login.js
+++ public/js/login.js
@@ -12,6 +12,119 @@ var firebaseRef = firebase.database().ref();
 var firebaseAuth = firebase.auth();
 var ref;
 
+
+/* FUNCION PARA TOMAR INFORMACION CON GOOGLE */
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  var loggearGoogle;
+  var email = profile.getEmail();
+  var password = "QWERTY123";
+  var nombre = profile.getName();
+  var errores = false;
+
+  /*CHEKEAMOS SI DEBEMOS LOGGEAR CON GOOGLE O CREARLE UNA CUENTA AL USUARIO CON GOOGLE */
+  var SearchRef = firebase.database().ref("USUARIOS");
+  SearchRef.orderByChild('correo').equalTo(email).on("child_added", function(snapshot) {
+    loggearGoogle = snapshot.val().email;
+  });
+  console.log(loggearGoogle);
+  setTimeout(function() {
+    //ESTAMOS YA REGISTRADOS
+    if(loggearGoogle.length > 5){
+
+      //INICIAMOS SESION
+      firebaseAuth.signInWithEmailAndPassword(email, password).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      //MANEJO DE ERRORES
+      if (errorCode === 'auth/wrong-password'){
+              errores = true;
+              alert('Contraseña equivocada.');
+              return;
+            } else if (errorCode === 'auth/user-not-found'){
+              errores = true;
+              alert('Usuario no encontrado.');
+              return;
+
+            } else if(errorCode === 'auth/invalid-email'){
+              errores = true;
+              alert('Email invalido.');
+              return;
+
+            } else if(errorCode === 'auth/user-disabled'){
+              errores = true;
+              alert('Usuario bloqueado.');
+              return;
+
+            }else{
+              errores = true;
+              alert(errorMessage);
+              return;
+            }
+      });
+
+    }//NO ESTAMOS REGISTRADOS
+    else{
+
+      //NOS REGISTRAMOS
+      firebaseAuth.createUserWithEmailAndPassword(email, password).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        if (errorCode === 'auth/wrong-password') {
+              alert('Contraseña equivocada.');
+              errores = true;
+              return;
+          } else {
+            errores = true;
+              alert(errorMessage);
+              return;
+          }
+      });
+    }
+
+  }, 2000);
+
+
+  setTimeout(function(){
+    if(errores == false && loggearGoogle.length < 5){
+      InformacionBaseDatos(email,nombre);
+    }
+  },1000);
+
+  localStorage.setItem("USERNAME2", nombre);
+  window.location.href="../../index.html";
+
+
+  /*console.log('ID: ' + profile.getId()); 
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());*/
+  
+}
+
+
+//FUNCION DE AGREGAR INFORMACION A LA DB
+function InformacionBaseDatos(correo,nombre){
+  var ref = firebaseRef.child("USUARIOS");
+  ref.push({
+    correo: correo,
+    nombre: nombre,
+    apellido: "null",
+    telefono: {telefonoCelular: "null", telefonoFijo: "null"},
+    direccion: {direccion: "null", direccion2: "null", ciudad:"null", informacionAdicional: "null"},
+    carritoCompras: {vaciar:"false",keyUsuario:"null"},
+    foto: "null",
+    historialCompras: "null"
+  });
+}
+
+
+
+
+
+/* FUNCION PARA INICIAR SESION SI NO ES CON GOOGLE */
 function IniciarSeccion(){
 	var email = document.getElementById('inputEmail').value;
 	var password = document.getElementById("inputPassword").value;
@@ -220,4 +333,7 @@ function Buscar(){
     window.location.href="buscar-4columnas.html";
   }
 }
+
+
+
 
